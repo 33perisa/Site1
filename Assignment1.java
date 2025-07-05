45036874a729f53efd7c44db32dd7ddf8854684e
33perisa <149735166+33perisa@users.noreply.github.com>
2025-07-05 17:20:36 +0200

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+public class Assignment1 {
+    public static void main(String[] args) {
+        double r = 10;
+        double H = 5;
+        double Pi = 3.14;
+
+        double V = ( r * r ) * Pi * H;
+        System.out.println("Zapremina valjka: " + V)
+    }
+}
